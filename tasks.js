6331188bda4617f570d00de180568aa8b270288c
Jadelette <jade.a.corcoran@gmail.com>
2019-02-12 08:52:58 +0000

    app.use commented out

--- tasks.js
+++ tasks.js
@@ -1,11 +1,10 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
-
+//app.use(express.json());
 const dbService = require('./dbservice');
 
 app.get('/tasks', function (request, response) {
-
   dbService.getTasks().then(function(tasks){
     //we got the tasks ok
     response.json(tasks);
@@ -16,6 +15,17 @@ app.get('/tasks', function (request, response) {
     response.json(error);
   });
 })
+
+
+
+app.post('/tasks', function (request, response) {
+  console.log("you posted this task: " + request.body.description);
+
+  response.json({message: "You have posted a task."});
+});
+
+
+
 
 app.delete('/tasks/:taskId', function(request, response){
   const taskToBeDeleted = request.params.taskId;
